b0d878497780e2adf03633b70db32774543ee2c1
Tomaz Geddes de Ameida <tomazgda@icloud.com>
2025-11-21 22:57:41 +0000

    added front end with Incr_dom

--- engine.ml
+++ engine.ml
@@ -1,11 +1,12 @@
 (* The following program is an implementation of Conway's Game of Life
-   by the author (Tomaz Geddes de Almeida) *)
+   by Tomaz Geddes de Almeida *)
 
 (* A cell refers to a position which is a pair of integers. To create a
    set of cells (a board) we need to implement a module Cell that provides a method
    compare. We take the polymorphic compare from Stdlib. A cell has type Cell.t.*)
 module Cell = struct
   type t = int * int
+
   let compare = Stdlib.compare
 end
 
@@ -14,29 +15,27 @@ end
    Board inherits from using the functor Set.Make. Set.Make(Cell) returns the module with set
    operations corresponding to our Cell. *)
 module Board = struct
-  include Set.Make(Cell)
+  include Set.Make (Cell)
 
   let neighbours (x, y) =
-    List.concat_map (fun dx -> List.map (fun dy -> (x+dx, y+dy)) [-1;0;1]) [-1;0;1]
-    |> List.filter ((<>) (x,y))
-  
+    List.concat_map
+      (fun dx -> List.map (fun dy -> (x + dx, y + dy)) [ -1; 0; 1 ])
+      [ -1; 0; 1 ]
+    |> List.filter (( <> ) (x, y))
+
   let frequencies list =
-    let frequency l = fun e -> (e, List.length (List.filter ((=) e) l)) in
+    let frequency l = fun e -> (e, List.length (List.filter (( = ) e) l)) in
     List.map (frequency list) (List.sort_uniq Stdlib.compare list)
 
   let add_cells cells board =
     List.fold_left (fun ret cell -> add cell ret) board cells
-  
+
   let step board =
     let build_board b (cell, n) =
-      if n = 3 || (n = 2 && (mem cell board))
-      then add cell b
-      else b
+      if n = 3 || (n = 2 && mem cell board) then add cell b else b
     in
-  
-    to_list board
-    |> List.concat_map neighbours
-    |> frequencies
+
+    to_list board |> List.concat_map neighbours |> frequencies
     |> List.fold_left build_board empty
 end
 
@@ -48,7 +47,7 @@ module Gol = struct
   type t = Cons of Board.t * t Lazy.t
 
   let rec create board = Cons (board, lazy (create (Board.step board)))
-  
+
   let rec at_time t (Cons (hd, tl)) =
     if t = 0 then hd else at_time (t - 1) (Lazy.force tl)
 end
--- main.ml
+++ main.ml
@@ -1,25 +1,121 @@
-open Engine
+open Core
+open Async_kernel
+open Incr_dom
 
-let display_board b =
-  for y = 0 to 10 do
-    for x = 0 to 10 do
-      match (Board.mem (x,y) b) with
-      | true -> print_string "#"
-      | false -> print_string "."
-    done;
-    print_newline ()
-  done;
-  print_newline ()
+module App = struct
+  module Model = struct
+    type t = { board : Engine.Board.t; width : int; height : int }
+    [@@deriving fields, compare]
+
+    let cutoff = phys_equal
+  end
+
+  module Action = struct
+    type t = Tick | Resize of int * int | Click of int * int [@@deriving sexp]
+  end
+
+  (* State is for holding imperitive state. e.g.  connections to servers.
+     we aren't making use of it but it is required by Start_app.start *)
+  module State = struct
+    type t = unit
+  end
+
+  let initial_model =
+
+    let glider_gun =
+      Patterns.glider_gun
+      |> List.map ~f:(fun (x, y) -> (x + 1, y + 1))
+      (* shift glider gun by a bit *)
+    in
+
+    let starting_board =
+      Engine.Board.empty |> Engine.Board.add_cells glider_gun
+    in
+    
+    let open Js_of_ocaml in
+    {
+      Model.board = starting_board;
+      width = Dom_html.window##.innerWidth;
+      height = Dom_html.window##.innerHeight;
+    }
+
+  let on_startup ~schedule_action _model =
+    let open Js_of_ocaml in
+
+    (* Tick every 0.25s -- TODO: make async *)
+    every (Time_ns.Span.of_sec 0.25) (fun () -> schedule_action Action.Tick);
+
+    (* Listen for window resizing *)
+    Dom_html.window##.onresize :=
+      Dom_html.handler (fun _ ->
+          let w = Dom_html.window##.innerWidth in
+          let h = Dom_html.window##.innerHeight in
+          schedule_action (Action.Resize (w, h));
+          Js._true);
+
+    Deferred.unit
+
+  let create model ~old_model:_ ~(inject : Action.t -> unit Vdom.Effect.t) =
+    let open Incr.Let_syntax in
+    let%map apply_action =
+      let%map board = model >>| Model.board
+      and width = model >>| Model.width
+      and height = model >>| Model.height in
+      fun (action : Action.t) _ ~schedule_action:_ ->
+        match action with
+        | Tick -> { Model.board = Engine.Board.step board; width; height }
+        | Resize (w, h) -> { Model.board; width = w; height = h }
+        | Click (x, y) ->
+            let new_board =
+              if Engine.Board.mem (x, y) board then
+                Engine.Board.remove (x, y) board
+              else Engine.Board.add (x, y) board
+            in
+            { Model.board = new_board; width; height }
+    and view =
+      (* on the LHS of let%map board is a constant value; while on the RHS the board fetched from the model
+         is an incremental value *)
+      let%map board = model >>| Model.board
+      and width = model >>| Model.width
+      and height = model >>| Model.height in
+
+      let cell_size = 24 in
+
+      let cell x y =
+        let colour =
+          if Engine.Board.mem (x, y) board then "tomato" else "white"
+        in
+        let style =
+          let ( @> ) = Css_gen.( @> ) in
+          Css_gen.background_color (`Name colour)
+          @> Css_gen.width (`Px cell_size)
+          @> Css_gen.height (`Px cell_size)
+        in
+
+        Vdom.Node.td
+          [ Vdom.Node.text "" ]
+          ~attrs:
+            [
+              Vdom.Attr.style style;
+              Vdom.Attr.on_click (fun _ -> inject (Click (x, y)));
+            ]
+      in
+
+      let cols = width / cell_size in
+      let rows = height / cell_size in
+
+      let row y =
+        let cells = List.init cols ~f:(fun x -> cell x y) in
+        Vdom.Node.tr cells
+      in
+
+      Vdom.Node.div [ Vdom.Node.table (List.init rows ~f:row) ]
+    and model = model in
+
+    Component.create ~apply_action model view
+end
 
 let () =
-  let blinker = [(1,1);(1,2);(1,3)] in
-  let starting_board = Board.empty |> Board.add_cells blinker in
-  let all_life = Gol.create starting_board in
-
-  let rec loop t =
-    all_life |> Gol.at_time t |> display_board;
-    loop (t + 1)
-  in
-  
-  loop 0 
-
+  Start_app.start
+    (module App)
+    ~bind_to_element_with_id:"app" ~initial_model:App.initial_model
